4206a3e72e1bf89a8cbf14533ee7e3bb3c987c88
Miguel Valencia <valencia.miguel.41@gmail.com>
2026-03-02 23:23:14 -0500

    feat: update dependencies and enhance sidebar with new icons

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,15 @@
 "use client";
 
-import { useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import {
+  HomeIcon,
+  PersonIcon,
+  PersonAddIcon,
+  GearIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+} from "@primer/octicons-react";
 
 interface SidebarItem {
   name: string;
@@ -22,71 +29,23 @@ export default function Sidebar({ isExpanded, onToggle }: SidebarProps) {
     {
       name: "Dashboard",
       href: "/dashboard",
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
-        </svg>
-      ),
+      icon: <HomeIcon />,
     },
     {
       name: "Patients",
       href: "/patients/lookup",
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fillRule="evenodd"
-            d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
+      icon: <PersonAddIcon />,
     },
     {
       name: "Profile",
       href: "/profile",
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fillRule="evenodd"
-            d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
+      icon: <PersonIcon />,
     },
 
     {
       name: "Settings",
       href: "/settings",
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fillRule="evenodd"
-            d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
+      icon: <GearIcon />,
     },
     // Add more menu items as needed
   ];
@@ -105,26 +64,7 @@ export default function Sidebar({ isExpanded, onToggle }: SidebarProps) {
             className="p-2 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
             aria-label={isExpanded ? "Collapse sidebar" : "Expand sidebar"}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              {!isExpanded ? (
-                <path
-                  fillRule="evenodd"
-                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                  clipRule="evenodd"
-                />
-              ) : (
-                <path
-                  fillRule="evenodd"
-                  d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                  clipRule="evenodd"
-                />
-              )}
-            </svg>
+            {!isExpanded ? <ChevronRightIcon /> : <ChevronLeftIcon />}
           </button>
         </div>
 
@@ -147,28 +87,6 @@ export default function Sidebar({ isExpanded, onToggle }: SidebarProps) {
             ))}
           </ul>
         </nav>
-
-        <div className="p-4 border-t dark:border-gray-800">
-          <button
-            className={`${
-              isExpanded ? "justify-center" : "justify-start"
-            } w-full flex items-center p-2 text-red-500 hover:bg-red-50 dark:hover:bg-red-900/20 rounded-md transition-colors`}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
-                clipRule="evenodd"
-              />
-            </svg>
-            {!isExpanded && <span className="ml-2">Logout</span>}
-          </button>
-        </div>
       </div>
     </div>
   );
---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export * from "./Button";
+export * from "./FormikInputFields";
+export * from "./Header";
+export * from "./HeaderCard";
+export * from "./InputFields";
+export * from "./MainLayout";
+export * from "./Sidebar";
